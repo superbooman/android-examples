d6de7f9a4f0ce61122c647cd32302c289877c5d5
DESKTOP-DTHLF58\T Sam <tysamay@gmail.com>
2021-12-16 23:20:12 -0500

    change

--- AudioRecording/app/src/main/java/github/nisrulz/audiorecording/MainActivity.java
+++ AudioRecording/app/src/main/java/github/nisrulz/audiorecording/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //wowser
 
         mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
         mFileName += "/audiorecord.3gp";
